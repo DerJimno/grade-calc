1258a6505428471980602bfc255fec72aec80a4a
Jimno <menaoir.jimmy@gmail.com>
2023-02-15 15:50:26 +0100

    Add html css js

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form')
+let all_notes = [];
+let result;
+
+
+
+
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(form)
+    for (const pair of formData.entries()) {
+      all_notes.push(pair[1])
+    }
+    // console.log(all_notes)
+    result = ((Number(all_notes[0]) * 3) + Number(all_notes[1]) + Number(all_notes[2]) + (Number(all_notes[3]) * 2) + (Number(all_notes[4]) * 3) + Number(all_notes[5]) + Number(all_notes[6]) + (Number(all_notes[7]) * 2) + Number(all_notes[8]) + Number(all_notes[9]) + Number(all_notes[10])) / 17
+    console.log(result)
+    document.getElementById('output').textContent = result.toFixed(2)
+
+    // form.addEventListener
+  })
+// console.log(result[1])
+// console.log(result[2])
