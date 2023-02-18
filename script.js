e18d9ec5380f3c6aa0dc2548853b4d7225960847
Jimno <menaoir.jimmy@gmail.com>
2023-02-18 13:33:50 +0100

    fix typeface fallback

--- script.js
+++ script.js
@@ -11,4 +11,4 @@ form.addEventListener('submit', (e) => {
     result = ((Number(all_notes[0]) * 3) + Number(all_notes[1]) + Number(all_notes[2]) + (Number(all_notes[3]) * 2) + (Number(all_notes[4]) * 3) + Number(all_notes[5]) + Number(all_notes[6]) + (Number(all_notes[7]) * 2) + Number(all_notes[8]) + Number(all_notes[9]) + Number(all_notes[10])) / 17
     console.log(result)
     document.getElementById('output').textContent = result.toFixed(2)
-  })
+})
